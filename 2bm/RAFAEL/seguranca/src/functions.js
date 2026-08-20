9720698a26d50244ca4dcc61ea46a42b230bea71
DiegoOliveira <202510164@unifoa.edu.br>
2026-08-20 18:40:22 -0300

    vt

--- 2bm/RAFAEL/seguranca/src/functions.js
+++ 2bm/RAFAEL/seguranca/src/functions.js
@@ -6,7 +6,7 @@ export function soma(a, b) {
 }
 
 export function fx(x){
-    if(typeof a === "number" && x>0){
+    if(typeof x === "number" && x>0){
         if(x==1) return false
         for(let i=2; 1<x; i++){
             if(x % i == 0){
